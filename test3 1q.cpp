e0f31c8d2f1ab778d87c2c60551cbc2ca6c28f2e
seshu786 <112849894+seshu786@users.noreply.github.com>
2022-09-06 09:43:40 +0530

    Add files via upload

---
+++ test3 1q.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c,d,e,f;
+	a=5;
+	b=3;
+	c=a+b;
+	d=a-b;
+	e=a/b;
+	f=a*b;
+	std::cout<<"c="<<c;
+	std::cout<<"d="<<d;
+	std::cout<<"e="<<e;
+	std::cout<<"f="<<f;
+	return 0;		
+}
+	
+
