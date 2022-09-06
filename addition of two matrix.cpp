ae3baaf32943d1f9d253524f9a11b3a044a17061
seshu786 <112849894+seshu786@users.noreply.github.com>
2022-09-06 10:44:56 +0530

    Add files via upload

---
+++ addition of two matrix.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a[2][2],b[2][2];
+	cout<<"provide first matrix";
+	for (i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+			cin>>a[i]{j};
+		}
+	}
+	cout<<"provide second matrix";
+	for(k=0;k<2;k++)
+	{
+		for(n=0;n<2;n++)
+		{
+			cin>>b[k][n];
+		}
+	}
+	}
+
