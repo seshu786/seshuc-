a4bd1c7b1bbe543005047d072e5654e721b71933
seshu786 <112849894+seshu786@users.noreply.github.com>
2022-09-07 12:32:26 +0530

    Add files via upload

---
+++ pattern.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char input, numarical = '1';
+
+    cout << "Enter the uppercase character you want to print in the last row: ";
+    cin >> input;
+
+    for(int i = 1; i <= (input-'1'+1); ++i)
+    {
+        for(int j = 1; j <= i; ++j)
+        {
+            cout << numarical << " ";
+        }
+        ++numarical;
+
+        cout << endl;
+    }
+    return 0;
+}
