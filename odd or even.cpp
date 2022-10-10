4d4b36e7ae0055fc8692191a4e272f7d35b9bbc4
seshu786 <112849894+seshu786@users.noreply.github.com>
2022-10-10 13:38:31 +0530

    Add files via upload

---
+++ odd or even.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"enter an integer:";
+	cin>>n;
+	
+	if(n%2==0)
+	cout<<"it is even number:"<<endl;
+	else
+	cout<<"it is odd number:";
+	
+	
+	return 0;
+}
